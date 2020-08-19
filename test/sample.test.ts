fbcac19a333c5d36bf785224110e0b036dddbab7
Andy R. Wyss <andreawyss@users.noreply.github.com>
2020-08-19 09:25:55 -0700

    Use SPDX-License-Identifier (#2)

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,8 +1,5 @@
-/*
- * Copyright (c) 2020 VMware, Inc. All Rights Reserved.
- * This software is released under MIT license.
- * The full license information can be found in LICENSE in the root directory of this project.
- */
+/* Copyright 2020 VMware, Inc.
+ * SPDX-License-Identifier: MIT */
 
 import { greeter, sum } from './sample';
 
